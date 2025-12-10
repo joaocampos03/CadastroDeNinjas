f5907bb2c427d254f784cc86cfab37d7169ca4cd
João Campos <joaomarcelo_1000@hotmail.com>
2025-12-09 21:40:41 -0300

    feat: Rota de findById adicionada

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -32,9 +32,9 @@ public class NinjaController {
     }
 
     //Mostrar ninja por ID (READ)
-    @GetMapping("/todosID")
-    public String mostrarTodosOsNinjasPorId() {
-        return "Ninja mostrado por id!";
+    @GetMapping("/listar/{id}")
+    public NinjaModel listarNinjasPorId(@PathVariable Long id) {
+        return ninjaService.listarNinjasPorId(id);
     }
 
     //Alterar dados dos ninjas (UPDATE)
